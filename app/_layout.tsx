f49765a6e7dd53ad2b79bf621bfa2e83102297e9
lucassperanzini <rm98297@fiap.com.br>
2026-05-14 00:46:40 -0300

    update architecture - bottom tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,38 @@
+import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { Stack } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function RootLayout() {
+
+
+
+
+
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-      <Stack.Screen name="welcome" />
-      <Stack.Screen name="signUp" />
-      <Stack.Screen name="(tabs)" />
-    </Stack>
+    // 1. O GestureHandlerRootView DEVE ter flex: 1
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      {/* 2. O BottomSheetModalProvider engloba o Stack */}
+      <BottomSheetModalProvider>
+        
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="welcome" />
+          <Stack.Screen name="signUp" />
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+        
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
   );
+  
+  // return (
+
+    
+  //   <Stack screenOptions={{ headerShown: false }}>
+  //     <Stack.Screen name="index" />
+  //     <Stack.Screen name="welcome" />
+  //     <Stack.Screen name="signUp" />
+  //     <Stack.Screen name="(tabs)" />
+  //   </Stack>
+  // );
 }
