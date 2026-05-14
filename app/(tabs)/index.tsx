804baad8524913ba8a5eab4db52239d812086cf8
lucassperanzini <rm98297@fiap.com.br>
2026-05-14 00:43:39 -0300

    add bottom sheet

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,41 @@
 import ContentCards from "@/components/HomeScreenComponents/ContentCards";
 import DoseCard from "@/components/HomeScreenComponents/DoseCard";
-import LiquidFillCard from "@/components/HomeScreenComponents/LiquidFillCard";
 import Header from "@/components/HomeScreenComponents/Header";
 import UploadButton from "@/components/HomeScreenComponents/UploadButton";
 import YourContents from "@/components/HomeScreenComponents/YourContents";
-import React, { useEffect, useRef } from "react";
-import {
-  Animated,
-  View
-} from "react-native";
+import { BottomSheetBackdrop, BottomSheetModal, BottomSheetView } from "@gorhom/bottom-sheet";
+import React, { useCallback, useEffect, useMemo, useRef } from "react";
+import { Animated, Text, View } from "react-native";
 import "../../global.css";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+
+
 
 export default function HomeScreen() {
   const fadeAnim = useRef(new Animated.Value(0)).current;
   const slideAnim = useRef(new Animated.Value(20)).current;
+
+  // 1. Ref para controlar o Bottom Sheet
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
+  // 2. Define as alturas que o Bottom Sheet pode assumir (ex: 25% e 50% da tela)
+  const snapPoints = useMemo(() => ['15%'], []);
+  // 3. Função disparada pelo seu Upload Button
+  const handlePresentModalPress = () => {
+    bottomSheetModalRef.current?.present();
+  };
+
+  const renderBackdrop = useCallback(
+    (props: any) => (
+      <BottomSheetBackdrop
+        {...props}
+        disappearsOnIndex={-1} // Fica invisível quando o modal fecha
+        appearsOnIndex={0}     // Aparece assim que o modal abre no primeiro snap point
+        pressBehavior="close"  // Garante que o toque fechará o modal
+      />
+    ),
+    []
+  );
+
 
   useEffect(() => {
     Animated.parallel([
@@ -39,20 +60,29 @@ export default function HomeScreen() {
         
           <Header/>
           <DoseCard onPress={() => {}} />
-
-            {/* TESTANDO  - Incluir no componente dos conteudos*/}
-          {/* <LiquidFillCard progress={60}   icon={
-                <MaterialCommunityIcons
-                name="brain"
-                size={30}
-                color="#a78bfa"
-                />
-            }title="Ola" status="Consolidado"/> */}
           <YourContents/>
           <ContentCards/>
-       
+      
         </Animated.View>
-        <UploadButton/>
+        <UploadButton onPress={handlePresentModalPress}/>
+
+
+
+
+
+             {/* O BOTTOM SHEET EM SI */}
+      <BottomSheetModal
+        ref={bottomSheetModalRef}
+        index={0} // abre no primeiro ponto
+        snapPoints={snapPoints}
+        backdropComponent={renderBackdrop}
+        backgroundStyle={{ backgroundColor: 'black' }}
+      >
+        <BottomSheetView style={{flex:1,alignItems:'center',padding:24}}>
+          <Text style={{color:"white"}}>Adicionar Conteúdo</Text>
+          {/* Ex: Botões de Câmera, Galeria, Arquivos */}
+        </BottomSheetView>
+      </BottomSheetModal>
            
     </View>
   );
