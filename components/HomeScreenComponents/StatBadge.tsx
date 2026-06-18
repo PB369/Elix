13ad811f0ba12243cf8d7228cfc3e81487e781ce
lucassperanzini <rm98297@fiap.com.br>
2026-06-18 15:55:52 -0300

    detail

--- components/HomeScreenComponents/StatBadge.tsx
+++ components/HomeScreenComponents/StatBadge.tsx
@@ -1,12 +1,19 @@
-import { Text, View } from "react-native";
+import { Text, Touchable, TouchableOpacity, View } from "react-native";
 
 const StatBadge = ({
   icon,
   value,
+
 }: {
   icon: React.ReactNode;
   value: string;
+
 }) => (
+
+<TouchableOpacity 
+  activeOpacity={0.7}   // ← opacidade ao pressionar: 0 = some, 1 = sem efeito
+>
+
   <View
     className="flex-row items-center min-h-10 px-3 py-1.5 ml-3 rounded-lg border border-white/10"
     style={{ backgroundColor: "rgba(255,255,255,0.07)" }}
@@ -14,6 +21,8 @@ const StatBadge = ({
     {icon}
     <Text className="text-white text-sm font-semibold ml-1.5">{value}</Text>
   </View>
+
+  </TouchableOpacity>
 );
 
 export default StatBadge
